bf94149455efd9192b456a342958e9d1ed615c92
Valkyrie Savage <valkyrie.savage@gmail.com>
2022-11-14 11:48:15 +0100

    Update demo.py
    typo

--- demo.py
+++ demo.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import tensorflow.v1.compat as tf
+import tensorflow.compat.v1 as tf
 import pickle
 import numpy as np
 from skimage import io,transform
